0779bde69cc28c2c0de39a9d16bb23beb59c7e14
ygraci <ygracianne@gmail.com>
2025-07-28 19:25:29 -1000

    Fully completed Project 1

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -2,37 +2,41 @@
 //your parameter variables go here!
 let birdFlyX = 20; //moves all birds left or right
 let birdFlyY = 10; //moves all birds up or down
-let sunRiseY = 40; //lets sun rise or lower
-let sunSize = 10;
-let sandNumber = 10000; //creates less or more sand 
-let dayNight = 200; //lets it be night beach or day beach
+let sunRiseY = 60; //lets sun rise or lower
+let sunSize = 20; //lets the sun get bigger 
+let sandNumber = 200; //creates less or more sand 
 let sandWidth = .5 //changes the width of the sand
 let sandHight =.5 //changes the hight of the sand
+let dayNight = 200; //lets it be night(100+) beach or day(0-99) beach if
+
+//line 88 sand color "let sand = color (242,210,169);"
+//line 100 & 101 sky colors "let skyDay = color (157,235,255); let skyNight = color (46,68,130);"
+//line 122 & 123 ocean colors "let oceanDay = color (41,164,195); let oceanNight = color (57,80,97);
+// line 144 & 145 sun colors "let sunDay = color (255,211,25); let sunNight = color (70,79,97);"
+// line 161 & 162 wave colors "let wavesDay = color (41,164,195); let wavesNight = color (57,80,97);"
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
+  //pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
+  pWallpaper.resolution(A3);
+  pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 0;
+  pWallpaper.grid_settings.row_offset  = 35;
 }
 
 function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
 }
 
-
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
   //setup - run once when the code is first starts
 
   sandBackDrop (0);//is inback of sandSpecks so the sandSpecks can be seen
-  //can change the arangement of the sand each time you refresh 
-  sandSpecks(sandNumber+400); // 10000 there is a total of 50000 ellipse in the 200x200 area 
+  sandSpecks(sandNumber+400); // gives a total amount for ellipse in the 200x200 area 
   sky (0);
   sun (99,sunRiseY+40,sunSize+80);
   ocean (0);
@@ -43,22 +47,22 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   
 
 }
+
 function sandSpecks(count){
   fill (0)
   for (let i = 0; i < count; i++) { 
     //i=0 means it starts with 0
     //i<count means it will have that many shapes 
-    let x = random(width);
-    let y = random(height); 
+    let x = random(0, 200);//gives random placements 
+    let y = random(130, 200);//gives random placements 
     let w = sandWidth+.2;
     let h = sandHight+.4;
-    ellipse(x, y, w, h);
-    //draws the ellipse using the x, y, w, h
+    ellipse(x, y, w, h);//draws the ellipse using the x, y, w, h
   }
 }
 
-
 function bird (birdX, birdY) {
+//code to make the bird go away at night
  let skyNight = color (46,68,130);
 
   angleMode(DEGREES);
@@ -68,7 +72,7 @@ function bird (birdX, birdY) {
   else{
     fill (skyNight);
   }
-
+//code to make the birds 
 //left wing
   arc (birdX+23, birdY+9.5, 30, 19, 290, 350, CHORD);
 
@@ -78,7 +82,6 @@ function bird (birdX, birdY) {
 //right wing
   arc (birdX+50, birdY+7, 29, 17, 180, 230, CHORD);
 }
-  
 
 function sandBackDrop () {
 noStroke();
@@ -92,9 +95,8 @@ beginShape();
 endShape(CLOSE);
 }
 
-
 function sky () {
-//sky
+//code to make it change to night and day
   let skyDay = color (157,235,255);
   let skyNight = color (46,68,130);
 
@@ -116,8 +118,9 @@ endShape(CLOSE);
 }
 
 function ocean () {
+//code to make it change to night and day
 let oceanDay = color (41,164,195);
-  let oceanNight = color (57,80,97);
+let oceanNight = color (57,80,97);
 
   if (dayNight > 100) {
     fill (oceanDay);
@@ -125,8 +128,9 @@ let oceanDay = color (41,164,195);
   else{
     fill (oceanNight);
   }
-//middle/top of the ocean
 
+//code to make the ocean
+//middle/top of the ocean
 beginShape();
   vertex (0,+80);//top left 
   vertex (200,80);//top right
@@ -136,13 +140,24 @@ endShape(CLOSE);
 }
 
 function sun (sunX, sunY, sunW) {
+//code to make it change to night and day
+let sunDay = color (255,211,25);
+let sunNight = color (70,79,97);
+
+  if (dayNight > 100) {
+    fill (sunDay);
+  }
+  else{
+    fill (sunNight);
+  }
+
+//code to make sun
   angleMode(DEGREES);
-  var sun = color (255,211,25);
-fill (sun);
 arc(sunX, sunY, sunW, 65, 180, 360);
 }
 
 function waves(){
+//code to make it change to night and day
   let wavesDay = color (41,164,195);
   let wavesNight = color (57,80,97);
 
@@ -153,6 +168,7 @@ function waves(){
     fill (wavesNight);
   }
 
+//code to make waves
 //1st waves
 beginShape();
   curveVertex(0,110); //top left point
